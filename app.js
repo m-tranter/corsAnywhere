8412129cbc565f4b7c6e2666f2860d807dc4402b
mark <tranter.m@sky.com>
2022-09-21 17:54:02 +0100

    removed a thing

--- app.js
+++ app.js
@@ -6,8 +6,7 @@ var port = process.env.PORT || 8080;
 
 var cors_proxy = require('cors-anywhere');
 cors_proxy.createServer({
-    originWhitelist: ['https://managementapi.onrender.com'],
-    requireHeader: ['origin', 'x-requested-with'],
+    originWhitelist: [],
     removeHeaders: ['cookie', 'cookie2']
 }).listen(port, host, function() {
     console.log('Running CORS Anywhere on ' + host + ':' + port);
