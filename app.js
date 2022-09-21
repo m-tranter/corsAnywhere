c44ffed5941a5533ccfc2409604ccbbb57c02ea2
mark <tranter.m@sky.com>
2022-09-21 17:38:40 +0100

    added whitelist

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var port = process.env.PORT || 8080;
 
 var cors_proxy = require('cors-anywhere');
 cors_proxy.createServer({
-    originWhitelist: [], // Allow all origins
+    originWhitelist: ['https://managementapi.onrender.com'],
     requireHeader: ['origin', 'x-requested-with'],
     removeHeaders: ['cookie', 'cookie2']
 }).listen(port, host, function() {
